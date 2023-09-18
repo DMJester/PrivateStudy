6bf9d384ea51256a477dd3e2e2b541bcc74bae16
DMJester <xmentomen@naver.com>
2023-09-18 16:51:33 +0900

    두 수의 합 - 백준

---
+++ 10950/10950.py
@@ -0,0 +1,12 @@
+#https://www.acmicpc.net/problem/10950
+
+import sys
+
+T = int(sys.stdin.readline().strip())
+res = [ 0 for i in range(T)]
+
+for i in range(T):
+    res[i] = sum(map(int, sys.stdin.readline().strip().split()))
+    
+for r in res:
+    print(r)
