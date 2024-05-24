ce62621ceec82565cdff13c3defce3ccbda2277e
게임하는도마뱀 <xmentomen@naver.com>
2024-05-24 09:22:55 +0900

    백준 미완성 코드

--- BFS&DFS/13144/13144.py
+++ BFS&DFS/13144/13144.py
@@ -5,19 +5,19 @@ sys.stdin = StringIO("".join(open("./BFS&DFS/13144/input.txt", "r").readlines())
  
 N = int(sys.stdin.readline().strip())
 numbers = list(map(int, sys.stdin.readline().strip().split()))
-number_use = [False for i in range(100002)]
+number_use = [False for i in range(100001)]
 res = 0
 
-for start in range(N):
-    now = start
-    while now < N:
-        if number_use[numbers[now]] == False:
-            number_use[numbers[now]] = True
-            now += 1
-            res += 1
-        else:
-            break
-    for idx in range(start, now):
-        number_use[numbers[idx]] = False
+start = 0
+end = 0
+cnt = 0
 
+while start < N:
+    while start <= end and end < N:
+        if number_use[numbers[end]] == False:
+            number_use[numbers[end]] = True
+            end += 1
+        else:
+            number_use[numbers[start]] = False
+            start += 1
 print(res)
