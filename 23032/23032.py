ad97ee60ef2a5bc1d546e8d0b37d6e628d57487a
게임하는도마뱀 <xmentomen@naver.com>
2024-07-11 19:04:03 +0900

    백준 서프라이즈~ 문제 소스 추가

--- 23032/23032.py
+++ 23032/23032.py
@@ -7,36 +7,38 @@ N = int(sys.stdin.readline())
 W = list(map(int, sys.stdin.readline().strip().split()))
 res = 0
 
-tl = []
-tr = []
-
 prev = 10000 * 2000
 for mid in range(1, N):
     left = mid - 1
     right = mid
-    ls = 0
-    rs = 0
-    
-    tl.clear()
-    tr.clear()
-    
-    while left >= 0 and right <= N:
-        if left >= 0:
-            tl.append(W[left])
-            ls += W[left]
-            left -= 1
-            
-        if right < N:
-            tr.append(W[right])
-            rs += W[right]
-            right += 1
-            
-        test = ls + rs
-        test = 0
-          
-        diff = abs(ls - rs)   
-        if diff <= prev:
+    left_sum = 0
+    right_sum = 0
+
+    left_sum += W[left]
+    right_sum += W[right]
+    while True:
+        diff = abs(left_sum - right_sum)   
+        if diff < prev:
             prev = diff
-            res = max(res, (ls+rs))
+            res = (left_sum+right_sum)
+        elif diff == prev:
+            prev = diff
+            res = max(res, (left_sum+right_sum))
+        
+        if left_sum < right_sum and left > 0:
+            left -= 1
+            left_sum += W[left]
+        elif left_sum > right_sum and right < N-1:
+            right += 1
+            right_sum += W[right]
+        elif left_sum == right_sum:
+            if left > 0:
+                left -= 1
+                left_sum += W[left]
+            if right < N-1:
+                right += 1
+                right_sum += W[right]
+        else:
+            break
 
 print(res)
