68549a8211ccfe7cf4979f28ff7f9dbb34fbb7f2
게임하는도마뱀 <xmentomen@naver.com>
2024-08-23 08:30:41 +0900

    미완성 문제 소스 추가

---
+++ Parametric Search/2343/2343.py
@@ -0,0 +1,31 @@
+#https://www.acmicpc.net/problem/2343
+import sys
+from io import StringIO
+
+sys.stdin = StringIO("".join(open("./Parametric Search/2343/input.txt", "r").readlines()))
+
+N, M = map(int, sys.stdin.readline().strip().split())
+course = list(map(int, sys.stdin.readline().strip().split()))
+course.append(0)
+res = 0
+
+min_time = 1
+max_time = (100000*10000)+1
+while min_time <= max_time:
+    set_time = (min_time+max_time) // 2
+    check_time = 0
+    disc_cnt = 0
+    
+    for time in course:
+        if check_time + time >= set_time:
+            disc_cnt += 1
+            check_time = 0
+        check_time += time
+            
+    if disc_cnt >= M:
+        min_time = set_time + 1
+        res = set_time
+    else:
+        max_time = set_time - 1
+            
+print(res)
