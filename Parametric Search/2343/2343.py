ab4eec603dcbe2f74a5ebf7832e5cf4d6c103bba
게임하는도마뱀 <xmentomen@naver.com>
2024-08-23 20:57:25 +0900

    백준 기타 레슨 문제 소스 추가

--- Parametric Search/2343/2343.py
+++ Parametric Search/2343/2343.py
@@ -6,28 +6,33 @@ sys.stdin = StringIO("".join(open("./Parametric Search/2343/input.txt", "r").rea
 
 N, M = map(int, sys.stdin.readline().strip().split())
 course = list(map(int, sys.stdin.readline().strip().split()))
-course.append(0)
 res = 0
 
-min_time = 1
-max_time = (100000*10000)+1
+min_time = max(course)
+max_time = sum(course)
 while min_time <= max_time:
     set_time = (min_time+max_time) // 2
+    highest_time = 0
     check_time = 0
     disc_cnt = 0
-    
+
     for time in course:
         check_time += time
         if check_time > set_time:
             disc_cnt += 1
+            highest_time = max(highest_time, check_time-time)
             check_time = time
         elif check_time == set_time:
             disc_cnt += 1
+            highest_time = max(highest_time, check_time)
             check_time = 0
-        
+    
+    if check_time >= 1:
+        disc_cnt += 1
+    
     if disc_cnt <= M:
         max_time = set_time - 1
-        res = set_time
+        res = highest_time
     else:
         min_time = set_time + 1
             
