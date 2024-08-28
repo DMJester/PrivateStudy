2684badd98f6b60d230ef03d58154dd28e3a2939
DMJester <dmjester@gmail.com>
2024-08-28 16:14:41 +0900

    백준 미해결 문제 소스 추가

--- Parametric Search/2792/2792.py
+++ Parametric Search/2792/2792.py
@@ -6,12 +6,29 @@ sys.stdin = StringIO("".join(open("./Parametric Search/2792/input.txt", "r").rea
 
 N, M = map(int, sys.stdin.readline().strip().split())
 jewels = [int(sys.stdin.readline().strip()) for _ in range(M)]
+res = (1000000000 * M) + 1
 
 left = 1
 right = 1000000000 * M
 while left <= right:
     set_ea = ( left + right ) // 2
-    remain_sum = 0
+    kid_cnt = 0
+    envy = 0
     
     for j in jewels:
+        quotient, remain = divmod(j, set_ea)
+        kid_cnt += quotient
+        envy = set_ea
+        
+        while remain >= 1:
+            remain -= quotient
+            envy += 1
+
+    if kid_cnt >= N:
+        right -= 1
+        res = min(res, envy)
+    else:
+        left += 1
+print(res)
+        
         
