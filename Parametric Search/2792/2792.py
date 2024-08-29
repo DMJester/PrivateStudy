5e47ccde3f92b11510768118775dcc55610ad6ff
게임하는도마뱀 <xmentomen@naver.com>
2024-08-29 23:03:42 +0900

    백준 보석 상자 문제 소스 추가

--- Parametric Search/2792/2792.py
+++ Parametric Search/2792/2792.py
@@ -6,34 +6,26 @@ sys.stdin = StringIO("".join(open("./Parametric Search/2792/input.txt", "r").rea
 
 N, M = map(int, sys.stdin.readline().strip().split())
 jewels = [int(sys.stdin.readline().strip()) for _ in range(M)]
-res = (1000000000 * M) + 1
+max_cnt = max(jewels)
+res = max_cnt
 
 left = 1
-right = max(jewels)
+right = max_cnt
 while left <= right:
     set_ea = ( left + right ) // 2
     kid_cnt = 0
-    envy = 0
     
     for j in jewels:
-        if set_ea <= j:
-            quotient, remain = divmod(j, set_ea)
-        else:
-            kid_cnt = 0
-            envy = 0
-            break
-            
+        quotient, remain = divmod(j, set_ea)
         kid_cnt += quotient
-        envy = max(envy, set_ea)
         
-        while remain >= 1:
-            remain -= quotient
-            envy += 1
+        if remain >= 1:
+            kid_cnt += 1
 
-    if kid_cnt >= N:
+    if kid_cnt > N:
         left = set_ea + 1
-        res = min(res, envy)
     else:
         right = set_ea - 1
-print(res)
+        res = set_ea
         
+print(res)
