c067067c0ff09cb18a89637e230cc07968a4012b
게임하는도마뱀 <xmentomen@naver.com>
2024-12-19 08:10:09 +0900

    백준 미완성 소스 추가

--- DynamicProgramming/1446/1446.py
+++ DynamicProgramming/1446/1446.py
@@ -1,15 +1,18 @@
 #https://www.acmicpc.net/problem/1446
 import sys
 from io import StringIO
-import heapq
 
 sys.stdin = StringIO("".join(open("./DynamicProgramming/1446/input.txt", "r").readlines()))
 
 N, D = map(int, sys.stdin.readline().strip().split())
 shortcuts = [ list(map(int, sys.stdin.readline().strip().split())) for _ in range(N) ] 
-running_in_the_90s = [float('inf') for _ in range(10001)]
+running_in_the_90s = [i for i in range(D+1)]
 
-q = heapq()
-q = 
-
-print(graph)
+for sp, ep, dist in shortcuts:
+  for now in range(ep, D+1):
+      if ep == now:
+        running_in_the_90s[now] = min(running_in_the_90s[ep], running_in_the_90s[sp]+dist)
+      else:
+        running_in_the_90s[now] = running_in_the_90s[now-1] + 1
+      
+print(running_in_the_90s[D])
