a6358c4e08f45959a5a8a4f5a9c3e8539c717af4
DMJester <dmjester@gmail.com>
2024-12-24 16:16:12 +0900

    백준 기타리스트 문제 작성중 소스 추가

--- DynamicProgramming/1495/1495.py
+++ DynamicProgramming/1495/1495.py
@@ -6,7 +6,9 @@ sys.stdin = StringIO("".join(open("./DynamicProgramming/1495/input.txt", "r").re
 
 N, S, M = map(int, sys.stdin.readline().strip().split())
 V_list = list(map(int, sys.stdin.readline().strip().split()))
-dp_table = []
+dp_table = [[0 for _ in range(M)] for _ in range(N+1)]
+dp_table[0][S] = S
 
-print(N, S, M)
-print(V_list)
+for vIdx in range(1, N+1):
+  for vol in range(M):
+    
