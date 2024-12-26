e157053b6a5e4fa80b59ec5a6cb67bc71a199f76
DMJester <dmjester@gmail.com>
2024-12-26 11:20:47 +0900

    백준 기타리스트 미완성 소스 추가

--- DynamicProgramming/1495/1495.py
+++ DynamicProgramming/1495/1495.py
@@ -6,9 +6,28 @@ sys.stdin = StringIO("".join(open("./DynamicProgramming/1495/input.txt", "r").re
 
 N, S, M = map(int, sys.stdin.readline().strip().split())
 V_list = list(map(int, sys.stdin.readline().strip().split()))
-dp_table = [[0 for _ in range(M)] for _ in range(N+1)]
-dp_table[0][S] = S
+dp_table = [[False for _ in range(M+1)] for _ in range(N+1)]
+dp_table[0][S] = True
 
 for vIdx in range(1, N+1):
-  for vol in range(M):
-    
+  for vol in range(M+1):
+    if dp_table[vIdx-1][vol]:
+      minus = vol - V_list[vIdx-1]
+      plus = vol + V_list[vIdx-1]
+      
+      if minus >= 0:
+        dp_table[vIdx][minus] = True
+        
+      if plus <= M:
+        dp_table[vIdx][plus] = True
+
+res = -1
+for dp in dp_table[-1]:
+  if dp:
+    for y in dp_table:
+      for i, x in enumerate(y):
+        if x:
+          res = max(i, res)
+    break
+  
+print(res)
