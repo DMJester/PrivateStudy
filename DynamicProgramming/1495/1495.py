113a3daeb2ed3b3d0b0b501684dfc0ce0cd187fb
DMJester <dmjester@gmail.com>
2024-12-26 15:36:40 +0900

    백준 기타리스트 문제 소스 제출

--- DynamicProgramming/1495/1495.py
+++ DynamicProgramming/1495/1495.py
@@ -8,6 +8,7 @@ N, S, M = map(int, sys.stdin.readline().strip().split())
 V_list = list(map(int, sys.stdin.readline().strip().split()))
 dp_table = [[False for _ in range(M+1)] for _ in range(N+1)]
 dp_table[0][S] = True
+res = -1
 
 for vIdx in range(1, N+1):
   for vol in range(M+1):
@@ -21,13 +22,9 @@ for vIdx in range(1, N+1):
       if plus <= M:
         dp_table[vIdx][plus] = True
 
-res = -1
-for dp in dp_table[-1]:
-  if dp:
-    for y in dp_table:
-      for i, x in enumerate(y):
-        if x:
-          res = max(i, res)
+for idx in range(M, 0, -1):
+  if dp_table[-1][idx]:
+    res = max(idx, res)
     break
   
 print(res)
