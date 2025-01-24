5e2943466ddf326b34e1f2d7de367c78c13c7dc7
게임하는도마뱀 <xmentomen@naver.com>
2025-01-24 21:02:09 +0900

    백준 LCS 문제 소스 추가

--- DynamicProgramming/9251/9251.py
+++ DynamicProgramming/9251/9251.py
@@ -9,14 +9,13 @@ str2 = "2"
 str1 += sys.stdin.readline().strip()
 str2 += sys.stdin.readline().strip()
 
-dp_table = [[0 for _ in range(len(str1))] for __ in range(len(str2))]
+dp_table = [[0 for _ in range(len(str2))] for __ in range(len(str1))]
 
-for y in range(len(str1)):
-  for x in range(len(str2)):
+for y in range(1, len(str1)):
+  for x in range(1, len(str2)):
     if str1[y] == str2[x]:
       dp_table[y][x] = dp_table[y-1][x-1] + 1
     else:
-      dp_table[y][x] = max(dp_table[y-1][x-1], )
+      dp_table[y][x] = max(dp_table[y][x-1], dp_table[y-1][x])
       
-for d in dp_table:
-  print(d)
+print(dp_table[-1][-1])
