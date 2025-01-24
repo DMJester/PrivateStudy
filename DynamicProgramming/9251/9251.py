e45fc829aed33903b196077f746e6925fe4dd259
게임하는도마뱀 <xmentomen@naver.com>
2025-01-24 09:17:42 +0900

    9251 문제 해결 중 소스 추가

--- DynamicProgramming/9251/9251.py
+++ DynamicProgramming/9251/9251.py
@@ -4,14 +4,19 @@ from io import StringIO
 
 sys.stdin = StringIO("".join(open("./DynamicProgramming/9251/input.txt", "r").readlines()))
 
-str1 = sys.stdin.readline().strip()
-str2 = sys.stdin.readline().strip()
+str1 = "1"
+str2 = "2"
+str1 += sys.stdin.readline().strip()
+str2 += sys.stdin.readline().strip()
 
 dp_table = [[0 for _ in range(len(str1))] for __ in range(len(str2))]
 
 for y in range(len(str1)):
-  ny = y
-  cnt = 0
   for x in range(len(str2)):
-    if str1[ny] == str2[x]:
+    if str1[y] == str2[x]:
+      dp_table[y][x] = dp_table[y-1][x-1] + 1
+    else:
+      dp_table[y][x] = max(dp_table[y-1][x-1], )
       
+for d in dp_table:
+  print(d)
