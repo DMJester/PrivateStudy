75f591f26b2622d75822fd67064e5f576c90995f
게임하는도마뱀 <xmentomen@naver.com>
2025-05-18 15:18:22 +0900

    백준 전생슬연구자 문제소스 추가

---
+++ Greedy/1826/1826.py
@@ -0,0 +1,31 @@
+#https://www.acmicpc.net/problem/1826
+#연료 채우기
+
+import sys
+from io import StringIO
+import heapq
+
+sys.stdin = StringIO("".join(open("./Greedy/1826/input.txt", "r").readlines()))
+
+T = int(sys.stdin.readline().strip())
+
+for idx in range(T):
+  N = int(sys.stdin.readline().strip())
+  Slimes = list(map(int, sys.stdin.readline().strip().split()))
+  heapq.heapify(Slimes)
+  step = []
+  res = 1
+  
+  if N == 1:
+    print(res)
+    continue
+  
+  while len(Slimes) >= 2:
+    Slime1 = heapq.heappop(Slimes)
+    Slime2 = heapq.heappop(Slimes)
+    
+    Slimes.append( Slime1 * Slime2 )
+    step.append(Slime1 * Slime2)
+  for s in step:
+    res *= s
+  print(res % 1000000007)
