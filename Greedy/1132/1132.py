c4747b862e0fcdc9f9f0a54c8f4baba76a3182bc
DMJester <dmjester@gmail.com>
2025-07-01 14:40:42 +0900

    백준 합 문제 소스 작성중

--- Greedy/1132/1132.py
+++ Greedy/1132/1132.py
@@ -6,6 +6,29 @@ from io import StringIO
 
 sys.stdin = StringIO("".join(open("./Greedy/1132/input.txt", "r").readlines()))
 
-N = int(sys.stdin.readline())
+def AlpToNum( alp_str, alp_dict ):
+  return int("".join(str(alp_dict[s]) for s in alp_str))
 
-print(N)
+N = int(sys.stdin.readline())
+alp_strs = [[c for c in sys.stdin.readline().strip()] for __ in range(N)]
+alphabets = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 'J':0 }
+res = 0
+
+for alp_str in alp_strs:
+  offset = len(alp_str)
+  for idx, alp in enumerate(alp_str):
+    alphabets[alp] += (10 ** offset)
+    offset -= 1
+
+sorted_alphabets = sorted(alphabets.items(), key=lambda x: x[1], reverse=True)
+
+set_value = 9
+for key, val in sorted_alphabets:
+  alphabets[key] = set_value
+  set_value -= 1
+
+for alp_str in alp_strs:
+  print(AlpToNum(alp_str, alphabets))
+  res += AlpToNum(alp_str, alphabets)
+
+print(res)
