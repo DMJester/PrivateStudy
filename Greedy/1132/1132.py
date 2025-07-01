750f8aa130882d5ecd07992d6802708e5ec676fd
DMJester <dmjester@gmail.com>
2025-07-01 14:57:38 +0900

    백준 합 문제 소스 작성중

--- Greedy/1132/1132.py
+++ Greedy/1132/1132.py
@@ -12,23 +12,35 @@ def AlpToNum( alp_str, alp_dict ):
 N = int(sys.stdin.readline())
 alp_strs = [[c for c in sys.stdin.readline().strip()] for __ in range(N)]
 alphabets = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 'J':0 }
+first_alphabet = set()
 res = 0
 
 for alp_str in alp_strs:
   offset = len(alp_str)
   for idx, alp in enumerate(alp_str):
+    if idx == 0:
+      first_alphabet.add(alp)
     alphabets[alp] += (10 ** offset)
     offset -= 1
 
-sorted_alphabets = sorted(alphabets.items(), key=lambda x: x[1], reverse=True)
+sorted_alphabets = sorted(alphabets.items(), key=lambda x: x[1], reverse=False)
 
-set_value = 9
+set_value = 1
+set_zero = False
 for key, val in sorted_alphabets:
-  alphabets[key] = set_value
-  set_value -= 1
+  if val != 0:
+    if key in first_alphabet:
+      alphabets[key] = set_value
+      set_value += 1
+    else:
+      if not set_zero:
+        alphabets[key] = 0
+        set_zero = True
+      else:
+        alphabets[key] = set_value
+        set_value += 1
 
 for alp_str in alp_strs:
-  print(AlpToNum(alp_str, alphabets))
   res += AlpToNum(alp_str, alphabets)
 
 print(res)
