edbc24cca3bfb5167b7e5f7ae464c299ee17568e
DMJester <dmjester@gmail.com>
2025-07-31 15:12:56 +0900

    백준 흙길 보수하기 문제 소스 수정 중

--- Greedy/1911/1911.py
+++ Greedy/1911/1911.py
@@ -10,18 +10,13 @@ sys.stdin = StringIO("".join(open("./Greedy/1911/input.txt", "r").readlines()))
 N, L = map(int, sys.stdin.readline().strip().split())
 pools = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
 pools.sort(key=lambda x:x[0])
-
 res = 0
 
-pool_start = 0
-pool_end = 0
+now = 0
 for p in pools:
-  if pool_end != p[0]-1:
-    plank -= (math.ceil((pool_end - pool_start) / L))
-    pool_start = p[0]
-    pool_end = p[1]
-  else:
-    pool_end = p[1]
-res += math.ceil((pool_end - pool_start) / L)
+  now = max(p[0], now)
+  plank = (math.ceil((p[1] - now)/L))
+  now += plank * L
+  res += plank
 
 print(res)
