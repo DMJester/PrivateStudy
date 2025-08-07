6c859e231ce816563aeaca120b299727f76d5010
DMJester <xmentomen@naver.com>
2025-08-07 19:58:44 +0900

    백준 같은 수로 만들기 문제 소스 추가

--- Greedy/2374/2374.py
+++ Greedy/2374/2374.py
@@ -7,7 +7,29 @@ from io import StringIO
 sys.stdin = StringIO("".join(open("./Greedy/2374/input.txt", "r").readlines()))
 
 N = int(sys.stdin.readline().strip())
-A_List = [int(sys.stdin.readline().strip()) for _ in range(N)]
+A_List = []
+res = 0
 
-for idx in N:
-  A_List
+prev_num = -1
+for idx in range(N):
+  now_num = int(sys.stdin.readline().strip())
+  if now_num != prev_num:
+    A_List.append(now_num)
+  prev_num = now_num
+  
+max_num = max(A_List)
+min_num = min(A_List)
+while min_num != max_num:
+  min_idx = A_List.index(min_num)
+  neighbor = []
+  
+  len_list = len(A_List)
+  if min_idx - 1 >= 0:
+    neighbor.append(A_List[min_idx-1])
+  if min_idx + 1 < len_list:
+    neighbor.append(A_List[min_idx+1])
+  
+  A_List.pop(min_idx)
+  res += min(neighbor) - min_num
+  min_num = min(A_List)
+print(res)
